d7f7538c26486c6794cfcab1cb39b7edbf745add
bharti <singhnishu540@gmail.com>
2023-07-04 00:29:02 +0530

    Quiz app for children. Using kotlin.

---
+++ app/src/main/java/com/example/mathquiz/QuestionList.kt
@@ -0,0 +1,73 @@
+package com.example.mathquiz
+
+import kotlin.random.Random
+
+class QuestionList(private val questionType: String?) {
+    private var questionList=ArrayList<Pair<String,Int>>(10)
+    private var questionDataList =ArrayList<Question>(10)
+    private var correctAnswer =""
+
+    private fun setQuestion()
+    {
+        when(questionType)
+        {
+            "easy"->{
+                for (i in 1..10)
+                    questionList.add(Easy.getQuestions())
+            }
+            "medium"-> {
+                for (i in 1..10)
+                    questionList.add(Medium.getQuestions())
+            }
+            else-> {
+                for (i in 1..10)
+                    questionList.add(Easy.getQuestions())
+            }
+        }
+    }
+    private fun getOption(position:Int):ArrayList<String>
+    {
+        val optionList =ArrayList<String>(4)
+        var answer =questionList[position].second
+        if (answer!=88888888)
+        {
+            correctAnswer =answer.toString()
+            optionList.add(answer.toString())
+            optionList.add((answer+Random.nextInt(-9,10)).toString())
+            optionList.add((answer+Random.nextInt(-9,10)).toString())
+            optionList.add("NA")
+        }
+        else
+        {
+            correctAnswer ="NA"
+            answer = Random.nextInt(1,4000)
+            optionList.add((answer+Random.nextInt(-9,10)).toString())
+            optionList.add((answer+Random.nextInt(-9,10)).toString())
+            optionList.add((answer+Random.nextInt(-9,10)).toString())
+            optionList.add("NA")
+        }
+        optionList.shuffle()
+        return optionList
+    }
+    fun getQuestionList():ArrayList<Question>
+    {
+        setQuestion()
+        for (i in 0..9)
+        {
+            val optionList =getOption(i)
+            val question=Question(
+                questionList[i].first,
+                correctAnswer,
+                optionList[0],
+                optionList[1],
+                optionList[2],
+                optionList[3],
+                "none"
+
+
+            )
+            questionDataList.add(question)
+        }
+        return questionDataList
+    }
+}
